6e53938ba7c7e8345e1f82fb3ca7f355c058cf40
Aliaksandr Zanouski <aliaksandr_zanouski@epam.com>
2014-10-06 12:04:02 +0300

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sampleAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sampleAppApp
+ */
+angular.module('sampleAppApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
